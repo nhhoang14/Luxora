9f83265a673482a240972c44a2da17d4e58f9174
nhhoang14 <nhhoangne@gmail.com>
2025-11-08 23:06:28 +0700

    fix settings

--- luxora/settings.py
+++ luxora/settings.py
@@ -1,7 +1,7 @@
 import os
 import shutil
-# import pymysql
-# pymysql.install_as_MySQLdb()
+import pymysql
+pymysql.install_as_MySQLdb()
 from pathlib import Path
 from decouple import config
 
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'luxora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '3306',
+    },
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    },
 }
 
 
