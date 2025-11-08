a60c220ad0258a16a4bbdefb62d8969c63820e96
Phongpronekk <caophongkun27.st@gmail.com>
2025-11-08 16:52:05 +0700

    model anh san pham

--- products/models.py
+++ products/models.py
@@ -29,6 +29,17 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
         ordering = ['order', 'name']
 
+# Model cho ảnh sản phẩm
+class ProductImage(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
 
 # Sản phẩm
 class Product(models.Model):
