62d7665da3bdebff468224e8fabbbede500dc432
nhhoang14 <nhhoangne@gmail.com>
2025-11-09 10:22:44 +0700

    final

--- orders/models.py
+++ orders/models.py
@@ -14,6 +14,7 @@ class Order(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, 
         on_delete=models.SET_NULL,
+        null=True,
         blank=True,
         related_name='orders'
     )
